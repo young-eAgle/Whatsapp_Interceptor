09618c5caf9ca906af551a054abae518a6da9219
young-eAgle <102895404+young-eAgle@users.noreply.github.com>
2024-12-13 13:46:55 +0500

    Added Backend

--- api/index.js
+++ api/index.js
@@ -7,6 +7,9 @@ const app = express();
 
 app.use(express.json());
 
+
+
+
 // Here express.json is built in middle ware that parses the incoming json making the body available in the req.body.
 
 app.use(
